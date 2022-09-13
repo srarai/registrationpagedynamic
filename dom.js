47f71b41670bba9c9854c49804b5f50a5bfd730f
srarai <yclass6@gmail.com>
2022-09-14 00:48:55 +0530

    Task 6

--- dom.js
+++ dom.js
@@ -49,7 +49,7 @@ headerTitle.style.borderBottom='solid 3px #000';
 // items[1].style.fontWeight = 'bold';
 // items[1].style.backgroundColor =' yellow';
 
-// //items.style.backgroundColor ='yellow'; // not gonaa work hence using for loop 
+// //items.style.backgroundColor ='yellow'; // not gonaa work hence because it is a html element  using for loop 
 
 // for (var i=0; i < items.length; i++){
 //     items[i].style.backgroundColor ='#f4f4f4';
@@ -74,14 +74,53 @@ li[1].textContent = 'Hello 2';
 li[1].style.fontWeight = 'bold';
 li[1].style.backgroundColor =' yellow';
 
-//items.style.backgroundColor ='yellow'; // not gonaa work hence using for loop 
 
 for (var i=0; i < li.length; i++){
     li[i].style.backgroundColor ='#f4f4f4';
 
 }
 
+// Task 6 
+// Query selector is used for only 1 item at once
+// if there are many item it always apply the change to the first item otherwise need to mention 
 
+var header = document.querySelector('#main-header');
+header.style.borderBottom = 'solid 4px #ccc';
+
+var input = document.querySelector('input');
+input.value =' Hello world';
+
+var submit =document.querySelector('input[type="submit"]');
+submit.value="Send"
+
+var item =document.querySelector('.list-group-item');
+item.style.color ='red';
+
+var lastitem = document.querySelector('.list-group-item:last-child');
+lastitem.style.color='blue';
+
+var seconditem =document.querySelector('.list-group-item:nth-child(2)');
+seconditem.style.color = 'coral';
+seconditem.style.backgroundColor= 'green'; // changing the background color 
+
+var thirditem =document.querySelector('.list-group-item:nth-child(3)');
+thirditem.style.visibility='hidden'; // making 3rd element visible 
+
+//Query selector all 
+
+
+var title =document.querySelectorAll('.title');
+console.log(title);
+
+// making background color green of odd li 
+
+// var odd = document.querySelectorAll('li:nth-child(odd)');
+// for (var i=0; i<odd.length; i++){
+//     odd[i].style.backgroundColor= 'green';
+// }
+
+var lis = document.querySelectorAll('.list-group-item');
+lis[3].style.color= 'green';
 
 
 
