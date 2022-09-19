2e791d13062742b14340dc5df176917ca92109de
srarai <yclass6@gmail.com>
2022-09-19 15:23:38 +0530

    Task11

--- Task_10/main.js
+++ Task_10/main.js
@@ -5,6 +5,11 @@ function store(event){
     event.preventDefault();
     const name= event.target.name.value;
     const email=event.target.email.value;
-    localStorage.setItem('name',name);
-    localStorage.setItem('email',email);
+    const obj={
+        name, 
+        email
+    }
+    localStorage.setItem('userDetails',JSON.stringify(obj));
+    
+
 }
